66b64152d651baf5e518e5aa00582293ef6bba41
Mezage <65369162+Mezage@users.noreply.github.com>
2020-10-04 14:53:29 -0700

    Add files via upload

---
+++ Assets/Collision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour {
+	public movement plMov;
+	
+	void OnCollisionEnter (UnityEngine.Collision colInfo){
+		if (colInfo.gameObject.tag == "Obstacle"){
+			//Debug.Log("Hit obst!");
+			plMov.enabled = false;
+			
+			FindObjectOfType<GamMan>().GameOver();
+		}
+		
+	}
+}
